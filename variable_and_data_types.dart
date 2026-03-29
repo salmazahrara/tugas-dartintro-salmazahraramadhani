73a806444b0c43febcd888f56377aac29a77456a
salma zahra <salmazahra897@gmail.com>
2026-03-30 00:07:06 +0700

    Add main function with variable declarations
    Initialize variables of different data types and print them.

---
+++ variable_and_data_types.dart
@@ -0,0 +1,13 @@
+void main() {
+  int umur = 20;           // bilangan bulat
+  double nilai = 90.5;     // bilangan desimal
+  String nama = "Budi";    // teks
+  bool isLulus = true;     // true atau false
+  var kota = "Semarang";   // otomatis deteksi tipe
+
+  print(nama);
+  print(umur);
+  print(nilai);
+  print(isLulus);
+  print(kota);
+}
