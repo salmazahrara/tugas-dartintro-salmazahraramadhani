5ebc21c152bbcb1727c7513afd7697b6d1baf57c
salma zahra <salmazahra897@gmail.com>
2026-03-30 00:09:28 +0700

    Add guided_practice.dart with student details

---
+++ guided_practice.dart
@@ -0,0 +1,9 @@
+void main() {
+  String nama = "Budi Santoso";
+  String nim = "A12345678";
+  String jurusan = "Informatika";
+
+  print("Nama    : $nama");
+  print("NIM     : $nim");
+  print("Jurusan : $jurusan");
+}
