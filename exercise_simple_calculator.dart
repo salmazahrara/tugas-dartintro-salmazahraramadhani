f4f19ef5ec6116d278d9056cac3bc10f3562352e
salma zahra <salmazahra897@gmail.com>
2026-03-30 00:10:35 +0700

    Implement simple calculator in Dart

---
+++ exercise_simple_calculator.dart
@@ -0,0 +1,18 @@
+void main() {
+  double a = 100;
+  double b = 3;
+
+  print('Angka 1       : $a');
+  print('Angka 2       : $b');
+  print('Penjumlahan   : ${a + b}');
+  print('Pengurangan   : ${a - b}');
+  print('Perkalian     : ${a * b}');
+  print('Pembagian     : ${a / b}');
+}
+
+// + = PERTAMBAHAN
+// - = PENGURANGAN
+// * = PERKALI
+// / = PEMBAGIAN --> TIPENYA DOUBLE 
+// ~/ = PEMBAGIAN HASILNYA INT
+// % SISA BAGI/ MODULO
